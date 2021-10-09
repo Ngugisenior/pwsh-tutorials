735a8c4fb05bc783b89aecf4e6254ea0674f16a3
ngugisenior <ngugijoseph992@gmail.com>
2021-10-09 03:14:02 +0200

    git commit

--- gt.ps1
+++ gt.ps1
@@ -1,6 +1,22 @@
-function commit($message, $branch) {
+function commit {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $message,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $branch
+    )
+
+    if (!$message){
+        $message = "updates"
+    }
+    if (!$branch){
+        $branch = "master"
+    }
+
+
     git add .
-    git commit -m '{$message}'
+    git commit -m $message
 
     git push -u origin $branch
     
