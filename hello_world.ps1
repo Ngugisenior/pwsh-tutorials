33f711014a8893f4eb5fd74e4a7ad9847c9bff02
ngugisenior <ngugijoseph992@gmail.com>
2021-10-09 02:46:48 +0200

    updates

---
+++ hello_world.ps1
@@ -0,0 +1,15 @@
+Write-Output "Hello World"
+
+""" print username """
+Write-Output "Hello "+ $env:USERNAME
+
+
+""" PowerShell Functions """
+function Receive-Output {
+    process { Write-Host $_ -ForegroundColor Green}
+    
+}
+""" Call Function ( Pipe result via function )"""
+Write-Output "Hello "+ $env:USERNAME | Receive-Output
+
+Write-Host "Hello "+ $env:USERNAME | Receive-Output
